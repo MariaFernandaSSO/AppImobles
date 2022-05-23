964cef8be403d025e80c41b1a44cb98d1c897e0b
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2022-05-23 10:57:33 +0000

    Conclusão da estrutura básica da classe Imobiliaria, onde foram implementados seus atributos e métodos construtor, gettters e setters

--- appNavegacaoOO/src/app/models/Imobiliaria.ts
+++ appNavegacaoOO/src/app/models/Imobiliaria.ts
@@ -1,3 +1,75 @@
 export class Imobiliaria {
+  private _nome : String 
+  private _endereco : String
+  private _cidade : String 
+  private _bairro: String
+  private _fone: String
+  private _site: String
 
+ constructor(nome: String, endereco: String, cidade: String, bairro: String, fone: String, site: String) {
+   this._nome = nome
+   this._endereco = endereco
+   this._cidade = cidade
+   this._bairro = bairro
+   this._fone = fone
+   this._site = site
+ }
+
+ // Nome
+ public set nome(nome: String){
+   this._nome = nome
+ }
+ 
+ public get nome(): String{
+   return this._nome
+}
+
+ // Endereco
+
+ public set endereco(endereco: String){
+   this._endereco = endereco
+ }
+
+ public get endereco(): String {
+   return this._endereco
+ }
+
+ // Cidade
+ 
+ public set cidade(cidade: String){
+   this._cidade = cidade 
+ }
+
+ public get cidade(): String {
+   return this._cidade
+ }
+
+ // Bairro
+
+ public set bairro(bairro: String){
+   this._bairro = bairro
+ }
+
+ public get bairro(): String {
+   return this._bairro
+ }
+ // Fone
+
+ public set fone(fone: String){
+   this._fone = fone
+ }
+
+ public get fone(): String {
+   return this._fone;
+ }
+
+ // Site
+
+ public set site(site: String){
+   this._fone = site
+ }
+
+ public get site(): String {
+   return this._site
+ }
 }
